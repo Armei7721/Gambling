b8d66dcabde08792015b3c507b0931b14507a4be
Armei7721 <hgjong23@naver.com>
2023-07-06 11:24:54 +0900

    Revert "20230705"
    This reverts commit 1ac225fc53559aa165165f4ea49ff2278fb46457.

--- Gamble/Assets/Scripts/Test3/Test.cs
+++ Gamble/Assets/Scripts/Test3/Test.cs
@@ -6,52 +6,52 @@ public class Test : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
 
+    
+    
     private int cardIndex;
     public GameObject test2;
     public GameObject test3;
+    //public GameObject test5;
     Test test;
     int cardCount = 0;
     private CardSprite card;
-
+    
     Dictionary<string, CardDictionary> cities = new Dictionary<string, CardDictionary>();
-
     void Start()
     {
+        card = GetComponent<CardSprite>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         cardIndex = Random.Range(0, 39);
-        card = test2.GetComponent<CardSprite>();
-
-        if (card != null)
-        {
-            Clone();
-        }
-        else
-        {
-            Debug.LogError("CardSprite component not found on test2 object.");
-        }
+        Clone();
     }
-
     // Update is called once per frame
     void Update()
     {
 
     }
-
     public void Clone()
     {
         if (cardCount < 1)
         {
             // 왼쪽에 카드 복제
-            GameObject cloneCardSprite = Instantiate(test2, transform.position - new Vector3(2f, 4f, 0f), Quaternion.identity);
+            test3 = Instantiate(test2, transform.position - new Vector3(2f, 4f, 0f), Quaternion.identity);
 
-            // 뽑힌 카드 가져오기
-            Sprite drawnCard = card.DrawCard();
+            // 복제된 오브젝트에 CardSprite 컴포넌트 추가
+            CardSprite cloneCardSpriteScript = test3.AddComponent<CardSprite>();
 
-            // 복제된 카드의 SpriteRenderer에 뽑힌 카드를 표시
-            cloneCardSprite.GetComponent<SpriteRenderer>().sprite = drawnCard;
+            if (cloneCardSpriteScript != null)
+            {
+                // 뽑힌 카드 가져오기
+                Sprite drawnCard = card.DrawCard();
+
+                // 복제된 카드의 SpriteRenderer에 뽑힌 카드를 표시
+                cloneCardSpriteScript.GetComponent<SpriteRenderer>().sprite = drawnCard;
+            }
 
             cardCount++;
             Debug.Log(cardCount);
         }
     }
+
+
 }
